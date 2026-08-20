d917e7de7ddca0d46e6f854fccc343625ceb3540
kpra3308-rgb <kpra3308@gmail.com>
2026-08-20 23:11:52 +0530

    fix scroll-to-top with useLayoutEffect

--- src/components/ui/ScrollToTop.tsx
+++ src/components/ui/ScrollToTop.tsx
@@ -1,14 +1,20 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import { usePathname } from "next/navigation";
 
 export default function ScrollToTop() {
   const pathname = usePathname();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
+
+  useEffect(() => {
+    const handleBeforeUnload = () => window.scrollTo(0, 0);
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => window.removeEventListener("beforeunload", handleBeforeUnload);
+  }, []);
 
   return null;
 }

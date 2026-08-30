1e9bd0444ee6849392282ebe25e4ad78f24e3319
kpra3308-rgb <kpra3308@gmail.com>
2026-08-30 23:58:48 +0530

    hero: revert to brand-story.jpg, NOT hero.jpg

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
     <section ref={ref} className="relative h-[100svh] w-full overflow-hidden bg-background">
       <motion.div style={{ y }} className="absolute inset-0">
         <Image
-          src="/hero.jpg"
+          src="/brand-story.jpg"
           alt="BONESYDE"
           fill
           priority
